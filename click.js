da1a61d9cffec65921a7fed7f0f771bd70278f31
JAYESH MULCHANDANI <98202868+jayeshmul@users.noreply.github.com>
2025-09-28 17:57:16 +0530

    Add files via upload
    Automation Router System Website

---
+++ click.js
@@ -0,0 +1,15 @@
+<section id="plans">
+    <h2>Our Plans</h2>
+    <div>
+        <h3 onclick="togglePlanInfo('plan1Info')">Prepaid Plan 1</h3>
+        <div id="plan1Info" style="display: none;">
+            <p>Details about Prepaid Plan 1.</p>
+        </div>
+    </div>
+    <div>
+        <h3 onclick="togglePlanInfo('plan2Info')">Prepaid Plan 2</h3>
+        <div id="plan2Info" style="display: none;">
+            <p>Details about Prepaid Plan 2.</p>
+        </div>
+    </div>
+</section>
